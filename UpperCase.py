d180a05a192c9d25a22649720fa42a7ace65a503
Julio <97416841+JulioEstrela@users.noreply.github.com>
2022-08-29 15:46:32 -0300

    Improvement

--- UpperCase.py
+++ UpperCase.py
@@ -1,14 +1,11 @@
 import string
 
-alphabet_up = frozenset(string.ascii_uppercase)
+text = input()
 
-def capital_indexes(text):
-    positions = [] 
-    for charPosition, char in enumerate(text):
-        if char in alphabet_up:
-            positions.append(charPosition)
-    
-    return positions
+positions = [] 
+
+for charPosition, char in enumerate(text):
+    if char.isupper():
+        positions.append(charPosition)
         
-positions = capital_indexes(input())
 print(positions)
